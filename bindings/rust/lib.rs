94a5577c350031daeb43a79984f91be34f0e562d
Stephan Seitz <stephan.seitz@fau.de>
2025-04-19 12:07:29 +0200

    bindings(rust)!: move to tree-sitter-language, move to Rust 2024 edition

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,32 +1,32 @@
 //! This crate provides slang language support for the [tree-sitter][] parsing library.
 //!
-//! Typically, you will use the [language][language func] function to add this language to a
+//! Typically, you will use the [LANGUAGE_RUST][] constant to add this language to a
 //! tree-sitter [Parser][], and then use the parser to parse some code:
 //!
 //! ```
+//! use tree_sitter_slang::LANGUAGE_SLANG;
+//!
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(&tree_sitter_slang::language()).expect("Error loading slang grammar");
+//! parser
+//!     .set_language(&LANGUAGE_SLANG.into())
+//!     .expect("Error loading SLANG language");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
-//! [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
-//! [language func]: fn.language.html
 //! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
 //! [tree-sitter]: https://tree-sitter.github.io/
 
-use tree_sitter::Language;
+use tree_sitter_language::LanguageFn;
 
-extern "C" {
-    fn tree_sitter_slang() -> Language;
+unsafe extern "C" {
+    fn tree_sitter_slang() -> *const ();
 }
 
-/// Get the tree-sitter [Language][] for this grammar.
+/// Get the tree-sitter [LanguageFn][] for this grammar.
 ///
-/// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
-pub fn language() -> Language {
-    unsafe { tree_sitter_slang() }
-}
+/// [LanguageFn]: https://docs.rs/tree-sitter-language/*/tree_sitter_language/struct.LanguageFn.html
+pub const LANGUAGE_SLANG: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_slang) };
 
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
@@ -42,11 +42,13 @@ pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
 #[cfg(test)]
 mod tests {
+    use crate::LANGUAGE_SLANG;
+
     #[test]
     fn test_can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
         parser
-            .set_language(&super::language())
-            .expect("Error loading slang language");
+            .set_language(&LANGUAGE_SLANG.into())
+            .expect("Error loading SLANG language");
     }
 }
